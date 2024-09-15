e783e1d5047e093fc91e409e38bd79f6b7351d4e
shub3am <shubhamvishwakarma0604@gmail.com>
2024-09-15 11:40:54 +0530

    working model

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import fs from "fs"
 const app = express();
 app.use(express.json())
-
 app.post("/query", async (req,res)=> {
 
     function fileToGenerativePart(path, mimeType) {
@@ -19,15 +18,16 @@ app.post("/query", async (req,res)=> {
 
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const {query} = req.body;
+    const allFiles = []
+
     const files = fs.readdirSync("./data")
-    const embeds = files.map(async file => {
-        return (fileToGenerativePart(`./data/${file}`, "application/pdf"))
-       
+    files.forEach(async file => {
+        let fa = (fileToGenerativePart(`./data/${file}`, "application/pdf"))
+        allFiles.push(fa)
       });
     // const BNS = fileToGenerativePart("./BNS.txt", "text/plain");
-      // console.log(allFiles)
 
-  const answer = await model.generateContent([`We have provided multiple pdf documents which is about the new Bhartiya Nyaya Sanhita (BNS) Act and THE BHARATIYA SAKSHYA BILL. The document is in English. You have to provide law consultation with the new documents and your knowledge and also compare the difference between old and new law system. \n\n Query: ${query}`, ...embeds ]);
+  const answer = await model.generateContent([`We have provided multiple pdf documents which has information about the new Bhartiya Nyaya Sanhita and THE BHARATIYA SAKSHYA BILL. The document is in English. You have to provide law consultation with the new documents and your knowledge and also compare the difference between old and new law system. \n\n Query: ${query}`, ...allFiles ]);
 
     if (!query) {
         res.json({error: "No query provided"})
